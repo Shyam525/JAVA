664630d4265a8432bcbe54ea6c461f00dd5445cb
Shyam525 <makvanashyam21@gmail.com>
2025-01-29 15:18:39 +0530

    first time

---
+++ studentchall.java
@@ -0,0 +1,15 @@
+
+
+
+
+public class studentchall{
+
+    public static final String gmail = null;
+    
+        public static void main(string[] args){
+    
+            String str ="programmer@gmail.com";
+    
+            int i = str.indexOf(gmail);
+    }
+}
